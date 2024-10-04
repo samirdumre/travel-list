5db0177f7d2d717759800950cb6cc7dd41b53e20
ayus-10 <136055230+ayus-10@users.noreply.github.com>
2024-10-04 21:32:25 +0545

    implement local storage to store data persistently

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Form from "./Components/Form.jsx";
 import Logo from "./Components/Logo.jsx";
 import PackagingList from "./Components/PackingList.jsx";
@@ -8,23 +8,49 @@ import Stats from "./Components/Stats.jsx";
 function App() {
     const [packingItems, setPackingItems] = useState([]);
 
+    useEffect(() => {
+        const localPackingItems = localStorage.getItem("PACKING_ITEMS");
+        if (localPackingItems) {
+            setPackingItems(JSON.parse(localPackingItems));
+        }
+    }, []);
+
+    function updateLocalPackingItems(packingItems) {
+        localStorage.setItem("PACKING_ITEMS", JSON.stringify(packingItems));
+    }
+
     function handleAddPackingItems(packingItem) {
-        setPackingItems((packingItems) => [...packingItems, packingItem]);
+        setPackingItems((packingItems) => {
+            const updatedPackingItems = [...packingItems, packingItem];
+            updateLocalPackingItems(updatedPackingItems);
+            return updatedPackingItems;
+        });
     }
 
     function handleDeletePackingItems(id) {
-        setPackingItems((packingItems) => packingItems.filter((packingItem) => packingItem.id !== id));
+        setPackingItems((packingItems) => {
+            const updatedPackingItems = packingItems.filter((packingItem) => packingItem.id !== id);
+            updateLocalPackingItems(updatedPackingItems);
+            return updatedPackingItems;
+        });
     }
 
     function handleTogglePackingItem(id) {
-        setPackingItems((packingItems) => (packingItems.map((packingItem) => packingItem.id === id ? {
-            ...packingItem, packed: !packingItem.packed
-        } : packingItem)))
+        setPackingItems((packingItems) => {
+            const updatedPackingItems = (packingItems.map((packingItem) => packingItem.id === id ? {
+                ...packingItem, packed: !packingItem.packed
+            } : packingItem));
+            updateLocalPackingItems(updatedPackingItems);
+            return updatedPackingItems;
+        })
     }
 
     function handleClearList() {
         const confirm = window.confirm("Are you sure you want to delete all items?");
-        if (confirm) setPackingItems(() => []);
+        if (confirm) setPackingItems(() => {
+            localStorage.removeItem("PACKING_ITEMS");
+            return [];
+        });
     }
 
     return (<div className={"app"}>
